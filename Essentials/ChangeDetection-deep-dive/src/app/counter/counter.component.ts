e3849509ca082ad8532274e2af0f9ea4c1431506
Vinod <vinodkumarmaddooru@gmail.com>
2025-05-01 17:38:33 +0530

    sample project for understanding the change detection strategy

---
+++ Essentials/ChangeDetection-deep-dive/src/app/counter/counter.component.ts
@@ -0,0 +1,41 @@
+import { Component, inject, NgZone, OnInit, signal } from '@angular/core';
+
+import { InfoMessageComponent } from '../info-message/info-message.component';
+
+@Component({
+  selector: 'app-counter',
+  standalone: true,
+  templateUrl: './counter.component.html',
+  styleUrl: './counter.component.css',
+  imports: [InfoMessageComponent],
+})
+export class CounterComponent implements OnInit {
+  count = signal(0);
+  private zone = inject(NgZone);
+
+  get debugOutput() {
+    console.log('[Counter] "debugOutput" binding re-evaluated.');
+    return 'Counter Component Debug Output';
+  }
+
+  ngOnInit(): void {
+    setTimeout(()=>{
+      console.log('[Counter] reset count');
+      this.count.set(0);
+    },4000)
+    this.zone.runOutsideAngular(()=>{
+      setTimeout(()=>{
+        console.log('timer after 5');
+      },6000)
+    })
+   
+  }
+
+  onDecrement() {
+    this.count.update((prevCount) => prevCount - 1);
+  }
+
+  onIncrement() {
+    this.count.update((prevCount) => prevCount + 1);
+  }
+}
